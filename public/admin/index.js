4122bac75737e12f2231ba0627e890a3e03d9a5d
khubmku@gmail.com <khubmku@gamil.com>
2025-12-30 15:46:32 +0530

    decap-cms-app

---
+++ public/admin/index.js
@@ -0,0 +1,7 @@
+import CMS from "decap-cms-app";
+
+// Optional: preview styles
+CMS.registerPreviewStyle("/styles/globals.css");
+
+// Optional: widgets
+// CMS.registerWidget("color", ColorControl, ColorPreview);
